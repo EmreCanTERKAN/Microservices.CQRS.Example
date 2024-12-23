8fe9d495e09044093e63acd98cf7a80ee7de5946
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-12-23 22:15:54 +0300

    düzeltnme

--- Microservices.CQRS.Example/MediatR_CQRS/Commands/Responses/CreateProductCommandResponse.cs
+++ Microservices.CQRS.Example/MediatR_CQRS/Commands/Responses/CreateProductCommandResponse.cs
@@ -2,7 +2,7 @@
 
 namespace Microservices.CQRS.Example.MediatR_CQRS.Commands.Responses
 {
-    public class CreateProductCommandResponse : IRequest<CreateProductCommandResponse>
+    public class CreateProductCommandResponse : 
     {
         public bool IsSuccess { get; set; }
         public Guid ProductId { get; set; }
